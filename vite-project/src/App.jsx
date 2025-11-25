a37b74873455bd8ea7014dbbc7ce6ad4d66751ca
Snehaparthipan <snehaparthipan123@gmail.com>
2025-11-25 16:44:44 +0000

    add style

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Home from './Components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar/>
+    <Home/>
+      {/* <Navbar/> */}
     </>
   )
 }
