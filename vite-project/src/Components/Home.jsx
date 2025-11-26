0088d5f108e9ade3253c141a5d682834790558a3
Snehaparthipan <snehaparthipan123@gmail.com>
2025-11-26 10:26:52 +0000

    icon

--- vite-project/src/Components/Home.jsx
+++ vite-project/src/Components/Home.jsx
@@ -5,8 +5,7 @@ import banvid from '../assets/Banvideo.mp4'
 export default function Home() {
   return (
     <>
-    <video src={banvid}></video>
-    <img src={img} alt="" />
+    <video src={banvid} muted autoPlay loop playsInline className='banvideo'></video>
     </>
   )
 }
